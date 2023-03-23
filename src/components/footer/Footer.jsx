dc238d55f370e6dae1911d7ca2d7e92a285499c5
Elyes-nk <elyesnk@gmail.com>
2023-03-23 18:31:56 +0100

    Edit footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Logo from "../../img/logo.png";
 import styles from "./footer.module.scss";
@@ -5,9 +6,9 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img src={Logo} alt="" />
+      <Image className={styles.logo} src={Logo} alt="" />
       <span>
-        Made with ❤️ and <b>React.js</b>.
+        Made with ❤️ and <b>React.js ⚡</b>.
       </span>
     </footer>
   );
